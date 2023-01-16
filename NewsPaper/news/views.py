f0be3a6b22e617c456c0c1b10f62cf693dbe846c
albinadesign <a-albina-s@ya.ru>
2023-01-16 10:08:11 +0300

    module d3
    reloaded

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# Импортируем класс, который говорит нам о том,
+# что в этом представлении мы будем выводить список объектов из БД
+from datetime import datetime
+
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class NewsList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = '-dateCreation'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    # queryset = Post.objects.all()
+    template_name = 'news.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'news'
+    paginate_by = 10
+
+
+class PostDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному посту
+    model = Post
+    # Используем другой шаблон — post.html
+    template_name = 'post.html'
+    # Название объекта, в котором будет выбранный пользователем пост
+    context_object_name = 'post'
